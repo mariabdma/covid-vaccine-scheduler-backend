391b02acbf92b551a56589fd56c292df52d63ad1
mariabdma <mariabma76@gmail.com>
2024-07-18 18:11:47 -0300

    [Bugfix]: Fixed appointment logic and added available hours function

--- src/routes/appointments.ts
+++ src/routes/appointments.ts
@@ -2,12 +2,13 @@ import { Router } from "express";
 import {
   createAppointment,
   getAppointments,
+  getAvailableHours,
 } from "../controllers/appointments";
 
 const router = Router();
 
 router.post("/", createAppointment);
-
 router.get("/", getAppointments);
+router.get("/available-hours", getAvailableHours);
 
 export default router;
